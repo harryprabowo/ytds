4bf7e705e30be19e5fa7bfaa38dea66174a6feac
Harry Prabowo <harryprabowo10@gmail.com>
2023-09-01 01:33:23 +0800

    Enjoy

--- components/rsvp/index.jsx
+++ components/rsvp/index.jsx
@@ -331,7 +331,6 @@ const RSVPForm = ({ venues, diets, submitRSVP }) => {
                         </Col>
                     </Tab.Container>
                 </Row>
-                <br />
                 <Row>
                     <Col style={{ textAlign: 'center' }}>
                         <Button type="submit" variant="warning" size="lg" disabled={loading || Object.keys(errors).length !== 0}>
